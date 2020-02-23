5613a9417139b7b41afdce820a649269a95847b9
ida <ida.czarniecka@gmail.com>
2020-02-23 15:23:13 +0100

    po zajęciach 23.02.2020

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import CreateArticle from "./components/create-article.component";
+import EditArticle from "./components/edit-article.component";
+import ArticleList from "./components/article-list.component"
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+
+          <Navbar.Brand>
+            <Link to={"/create-article"} className="nav-link">
+              React MERN Stack App
+            </Link>
+          </Navbar.Brand>
+
+          <Nav className="justify-content-end">
+            <Nav>
+              <Link to={"/create-article"} className="nav-link">
+                Create Article
+              </Link>
+              <Link to={"/article-list"} className="nav-link">
+                Article list
+              </Link>
+            </Nav>
+          </Nav>
+
+        </Container>
+      </Navbar>
+    </header>
+
+    <Container>
+      <Row>
+        <Col md={12}>
+          <div className="Wrapper">
+            <Switch>
+              <Route exact path='/' component={CreateArticle}/>
+              <Route path="/create-article" component={CreateArticle}/>
+              <Route path="/article-list" component={ArticleList}/>
+              <Route path="/edit-article/:id" component={EditArticle}/>
+            </Switch>
+
+
+          </div>
+        </Col>
+      </Row>
+    </Container>
     </div>
-  );
+  </Router>)
 }
 
 export default App;
